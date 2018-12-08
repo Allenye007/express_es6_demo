c8e8ffd1267e524f2c950a27a812ba9aec2a97d2
allen <allenye@aliyun.com>
2018-12-08 19:14:15 +0800

    拆表

--- models/ZxVenderPicArr.js
+++ models/ZxVenderPicArr.js
@@ -10,6 +10,12 @@ schema = {
         type: Sequelize.STRING,
         allowNull: false,
         primaryKey:true,
+    },
+    ui: {
+        type: Sequelize.STRING,
+    },
+    url: {
+        type: Sequelize.STRING,
     }
 }
 
@@ -20,4 +26,29 @@ sequelizeInstance.sync({force: false});
 
 
 
-module.exports = ZxVenderPicArr;
+// module.exports = ZxVenderPicArr;
+
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.resolve(__dirname, '../initData/vendor.json'), 'utf-8', (err, data) => {
+    // var len1 = JSON.parse(data).data.Items.length;
+    // var len2 = JSON.parse(data).data.Items[i].aptitude.length;
+    // console.log(JSON.parse(data).data.Items[1].aptitude);
+    var arr = [];
+    var obj= {};
+    for(var i = 0; i < 29; i++) {
+    
+        if(JSON.parse(data).data.Items[i].aptitude === undefined || JSON.parse(data).data.Items[i].aptitude <= 0) {
+
+        } else {
+            for(var k = 0; k < JSON.parse(data).data.Items[i].aptitude.length; k++) {
+                // arr.push(JSON.parse(data).data.Items[i].aptitude[k].url)
+                obj.url = JSON.parse(data).data.Items[i].aptitude[k].url;
+                obj.id = JSON.parse(data).data.Items[i].id;
+                arr.push(obj);
+            }
+        }
+        console.log(arr);
+    }
+})
